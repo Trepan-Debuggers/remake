daddb52ebc7d47031e03788749c7951369a92a3b
roland <roland>
1992-04-14 22:37:04 +0000

    Formerly make.h.~42~

--- make.h
+++ make.h
@@ -269,6 +269,6 @@ extern char *program;
 extern unsigned int makelevel;
 
 
-#define DEBUGPR(msg)							\
-  if (debug_flag) { print_spaces (depth); printf (msg, file->name);	\
-		    fflush (stdout);  } else
+#define DEBUGPR(msg) \
+  do if (debug_flag) { print_spaces (depth); printf (msg, file->name); \
+		       fflush (stdout); } while (0)
