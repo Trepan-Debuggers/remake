d572b1010373d3eee660c4cc8ece23c024171de7
roland <roland>
1992-04-15 06:02:36 +0000

    Formerly job.c.~58~

--- job.c
+++ job.c
@@ -161,10 +161,10 @@ child_error (target_name, exit_code, exit_sig, coredump, ignored)
      int exit_code, exit_sig, coredump;
      int ignored;
 {
-  char *ignore_string = ignored ? " (ignored)" : "";
-
   if (exit_sig == 0)
-    error ("*** [%s] Error %d%s", target_name, exit_code, ignore_string);
+    error (ignored ? "[%s] Error %d (ignored)" :
+	   "*** [%s] Error %d%s",
+	   target_name, exit_code);
   else
     {
       char *coredump_string = coredump ? " (core dumped)" : "";
