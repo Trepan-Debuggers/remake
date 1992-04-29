2c141e2a518c163988ea7646e9e28c3a2c02604c
roland <roland>
1992-04-29 21:22:58 +0000

    Formerly remake.c.~27~

--- remake.c
+++ remake.c
@@ -719,11 +719,11 @@ remake_file (file)
       else
 	{
 	  /* This is a dependency file we cannot remake.  Fail.  */
-	  static char noway[] = "*** No rule to make target";
+	  static char noway[] = "No rule to make target";
 	  if (keep_going_flag || file->dontcare)
 	    {
 	      if (!file->dontcare)
-		error ("%s `%s'.", noway, file->name);
+		error ("*** %s `%s'.", noway, file->name);
  	      file->update_status = 1;
 	    }
 	  else
