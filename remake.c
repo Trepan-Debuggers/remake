4e81bcd12b50fe2375a1ccaacd6e2333c27cdfbc
roland <roland>
1992-07-15 02:15:54 +0000

    Formerly remake.c.~31~

--- remake.c
+++ remake.c
@@ -224,7 +224,7 @@ update_file (file, depth)
 	  return 0;
 
 	default:
-	  error ("internal error: `%s' command_state == %d in update_file_1",
+	  error ("internal error: `%s' command_state == %d in update_file",
 		 f->name, (int) f->command_state);
 	  abort ();
 	  break;
