9c32114d605eedee6fdaab43551513e895dc591a
roland <roland>
1992-07-30 19:41:18 +0000

    Formerly make.h.~51~

--- make.h
+++ make.h
@@ -40,6 +40,10 @@ extern int errno;
 #endif
 #endif
 
+#if !defined (HAVE_SYS_SIGLIST) && defined (HAVE__SYS_SIGLIST)
+#define	sys_siglist	_sys_siglist
+#endif
+
 #ifndef	isblank
 #define	isblank(c)	((c) == ' ' || (c) == '\t')
 #endif
