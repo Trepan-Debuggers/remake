32e579293d6e1bd48124d6faef4b010168cfecbb
roland <roland>
1992-08-02 11:28:00 +0000

    Formerly make.h.~53~

--- make.h
+++ make.h
@@ -305,6 +305,3 @@ extern unsigned int makelevel;
 #define DEBUGPR(msg) \
   do if (debug_flag) { print_spaces (depth); printf (msg, file->name); \
 		       fflush (stdout); } while (0)
-
-/* !!! compile frob */
- int compiling;
