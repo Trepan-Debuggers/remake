2654b39389fed2573d7afcb052569257edc0b95b
roland <roland>
1992-10-25 05:49:07 +0000

    Formerly remake.c.~36~

--- remake.c
+++ remake.c
@@ -207,6 +207,12 @@ update_file (file, depth)
 	  d->file->command_state = f->command_state;
 	  d->file->update_status = f->update_status;
 	  d->file->updated = f->updated;
+	  if (debug_flag)
+	    {
+	      print_spaces (depth);
+	      printf ("File `%s' was also made by making `%s'.\n",
+		      d->file->name, f->name);
+	    }
 	}
 
       if (status != 0 && !keep_going_flag)
