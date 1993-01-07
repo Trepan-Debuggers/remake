a43b5ca4534057ccd196d89e1916b03edd86fd6b
roland <roland>
1993-01-07 01:46:47 +0000

    Formerly job.c.~88~

--- job.c
+++ job.c
@@ -757,7 +757,7 @@ load_too_high ()
       static int lossage = 0;
       /* Complain only once for the same error.  */
       if (lossage == 0 || errno != lossage)
-	perror_with_name ("cannot enforce load limit", "getloadavg");
+	perror_with_name ("cannot enforce load limit: ", "getloadavg");
       lossage = errno;
       load = 0;
     }
