255023f1a276608b82203724de01f8d9adbdbaf3
roland <roland>
1993-01-08 22:40:24 +0000

    Formerly job.c.~89~

--- job.c
+++ job.c
@@ -754,10 +754,16 @@ load_too_high ()
   make_access ();
   if (getloadavg (&load, 1) != 1)
     {
-      static int lossage = 0;
+      static int lossage = -1;
       /* Complain only once for the same error.  */
-      if (lossage == 0 || errno != lossage)
-	perror_with_name ("cannot enforce load limit: ", "getloadavg");
+      if (lossage == -1 || errno != lossage)
+	{
+	  if (errno == 0)
+	    /* An errno value of zero means getloadavg is just unsupported.  */
+	    error ("cannot enforce load limits on this operating system");
+	  else
+	    perror_with_name ("cannot enforce load limit: ", "getloadavg");
+	}
       lossage = errno;
       load = 0;
     }
