dd424872c06ff55f2e7e140bb9d13e3fabe6a9f3
roland <roland>
1993-01-13 21:18:56 +0000

    Formerly arscan.c.~26~

--- arscan.c
+++ arscan.c
@@ -1,5 +1,5 @@
 /* Library function for scanning an archive file.
-   Copyright (C) 1987, 1989, 1991, 1992 Free Software Foundation, Inc.
+   Copyright (C) 1987, 1989, 1991, 1992, 1993 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -97,7 +97,7 @@ ar_scan (archive, function, arg)
 #else
 #ifdef AIAMAG
   {
-    register int nread = read (desc, &fl_header, FL_HSZ);
+    register int nread = read (desc, (char *) &fl_header, FL_HSZ);
     if (nread != FL_HSZ || bcmp (fl_header.fl_magic, AIAMAG, SAIAMAG))
       {
 	(void) close (desc);
