70f443a4f78b79855d07b427dea59073830c0767
roland <roland>
1993-02-21 19:28:30 +0000

    Formerly variable.c.~19~

--- variable.c
+++ variable.c
@@ -548,7 +548,9 @@ target_environment (file)
    If not, NULL is returned.  */
 
 struct variable *
-try_variable_definition (line, origin)
+try_variable_definition (filename, lineno, line, origin)
+     char *filename;
+     unsigned int lineno;
      char *line;
      enum variable_origin origin;
 {
@@ -593,6 +595,14 @@ try_variable_definition (line, origin)
   name = savestring (beg, end - beg);
   expanded_name = allocated_variable_expand (name);
   free (name);
+
+  if (expanded_name[0] == '\0')
+    {
+      if (filename == 0)
+	fatal ("empty variable name");
+      else
+	makefile_fatal (filename, lineno, "empty variable name");
+    }
 
   v = define_variable (expanded_name, strlen (expanded_name),
 		       recursive ? p : variable_expand (p),
