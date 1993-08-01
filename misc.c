6b7709fcc3b1240abf4ef82ca5bb9f2a6eae9617
roland <roland>
1993-08-01 20:03:45 +0000

    Formerly misc.c.~27~

--- misc.c
+++ misc.c
@@ -509,9 +509,13 @@ log_access (flavor)
   if (! debug_flag)
     return;
 
-  printf ("%s access: user %d (real %d), group %d (real %d)\n",
-	  flavor, geteuid (), getuid (), getegid (), getgid ());
-  fflush (stdout);
+  /* All the other debugging messages go to stdout,
+     but we write this one to stderr because it might be
+     run in a child fork whose stdout is piped.  */
+
+  fprintf (stderr, "%s access: user %d (real %d), group %d (real %d)\n",
+	   flavor, geteuid (), getuid (), getegid (), getgid ());
+  fflush (stderr);
 }
 
 
