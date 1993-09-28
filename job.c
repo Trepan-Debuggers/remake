e974e439a6bfe4c9dbd801e3bdb4626c89edd69b
roland <roland>
1993-09-28 18:18:15 +0000

    Formerly job.c.~110~

--- job.c
+++ job.c
@@ -1179,7 +1179,7 @@ construct_command_argv_internal (line, restp, shell, ifs)
      char *line, **restp;
      char *shell, *ifs;
 {
-  static char sh_chars[] = "#;\"*?[]&|<>(){}$`";
+  static char sh_chars[] = "#;\"*?[]&|<>(){}$`^";
   static char *sh_cmds[] = { "cd", "eval", "exec", "exit", "login",
 			     "logout", "set", "umask", "wait", "while", "for",
 			     "case", "if", ":", ".", "break", "continue",
