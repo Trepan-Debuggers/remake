a68c073a8574a45f25e6c3489642cd5bce8befa2
roland <roland>
1994-04-20 22:42:32 +0000

    (read_makefile): Write no error msg for -include file.

--- read.c
+++ read.c
@@ -576,7 +576,8 @@ read_makefile (filename, flags)
 	      files = next;
 
 	      if (! read_makefile (name, (RM_INCLUDED | RM_NO_TILDE
-					  | (noerror ? RM_DONTCARE : 0))))
+					  | (noerror ? RM_DONTCARE : 0)))
+		  && ! noerror)
 		makefile_error (filename, lineno,
 				"%s: %s", name, strerror (errno));
 	    }
