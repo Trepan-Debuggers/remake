dc4c741579782d448c97a036770b06e83e0b8ff7
roland <roland>
1995-02-22 03:17:54 +0000

    (child_error): Use strsignal.

--- job.c
+++ job.c
@@ -174,14 +174,9 @@ child_error (target_name, exit_code, exit_sig, coredump, ignored)
 	   "*** [%s] Error %d",
 	   target_name, exit_code);
   else
-    {
-      char *coredump_string = coredump ? " (core dumped)" : "";
-      if (exit_sig > 0 && exit_sig < NSIG)
-	error ("*** [%s] %s%s",
-	       target_name, sys_siglist[exit_sig], coredump_string);
-      else
-	error ("*** [%s] Signal %d%s", target_name, exit_sig, coredump_string);
-    }
+    error ("*** [%s] %s%s",
+	   target_name, strsignal (exit_sig),
+	   coredump ? " (core dumped)" : "");
 }
 
 static unsigned int dead_children = 0;
