7be0bae64cbfb47856bf7ef9fbfc4ea440713cdb
roland <roland>
1995-10-10 18:59:08 +0000

    (enter_command_line_file): Barf if NAME is "".

--- main.c
+++ main.c
@@ -367,6 +367,9 @@ static struct file *
 enter_command_line_file (name)
      char *name;
 {
+  if (name[0] == '\0')
+    fatal ("empty string invalid as file name");
+
   if (name[0] == '~')
     {
       char *expanded = tilde_expand (name);
