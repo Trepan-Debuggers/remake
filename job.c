3b95d6a2f856a930154ebac2478897358cebd153
roland <roland>
1995-11-22 00:14:59 +0000

    (start_job_command): Pass null instead of "" to message; don't add newline.

--- job.c
+++ job.c
@@ -592,12 +592,12 @@ start_job_command (child)
       return;
     }
 
-  /* Print out the command.  If silent, we call `message' with an empty
-     string so it can log the working directory before the command's own
-     error messages appear.  */
+  /* Print out the command.  If silent, we call `message' with null so it
+     can log the working directory before the command's own error messages
+     appear.  */
 
   message ((just_print_flag || (!(flags & COMMANDS_SILENT) && !silent_flag))
-	   ? "" : "%s\n", p);
+	   ? (char *) 0 : "%s", p);
 
   /* Tell update_goal_chain that a command has been started on behalf of
      this target.  It is important that this happens here and not in
