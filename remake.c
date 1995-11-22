63d600242f55e2dd71b8fa1b105533feaeb0ad07
roland <roland>
1995-11-22 00:16:14 +0000

    (touch_file): Removing newline from message string.

--- remake.c
+++ remake.c
@@ -769,7 +769,7 @@ touch_file (file)
      register struct file *file;
 {
   if (!silent_flag)
-    message ("touch %s\n", file->name);
+    message ("touch %s", file->name);
 
 #ifndef	NO_ARCHIVES
   if (ar_name (file->name))
