735697aa311f5e52fd9a67f9a33dee1da959f101
roland <roland>
1995-11-26 01:38:10 +0000

    Pass new arg in `message' calls.

--- remake.c
+++ remake.c
@@ -195,14 +195,10 @@ update_goal_chain (goals, makefiles)
 		  && file->update_status == 0 && !g->changed
 		  /* Never give a message under -s or -q.  */
 		  && !silent_flag && !question_flag)
-		{
-		  if (file->phony || file->cmds == 0)
-		    message ("Nothing to be done for `%s'.",
-			     file->name);
-		  else
-		    message ("`%s' is up to date.", file->name);
-		  fflush (stdout);
-		}
+		message (1, ((file->phony || file->cmds == 0)
+			     ? "Nothing to be done for `%s'."
+			     : "`%s' is up to date."),
+			 file->name);
 
 	      /* This goal is finished.  Remove it from the chain.  */
 	      if (lastgoal == 0)
@@ -769,7 +765,7 @@ touch_file (file)
      register struct file *file;
 {
   if (!silent_flag)
-    message ("touch %s", file->name);
+    message (0, "touch %s", file->name);
 
 #ifndef	NO_ARCHIVES
   if (ar_name (file->name))
