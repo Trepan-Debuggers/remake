282e6495c4f74f6afba47560c27e2e1ce401ff81
roland <roland>
1995-11-26 01:42:27 +0000

    [HAVE_UNION_WAIT]: Include sys/wait.h. (start_job_command): Pass new arg to `message'; fix inverted test in that call.

--- job.c
+++ job.c
@@ -55,7 +55,7 @@ static int dos_batch_file;
 #endif	/* POSIX.  */
 #endif
 
-#ifdef	HAVE_SYS_WAIT_H
+#if defined (HAVE_SYS_WAIT_H) || defined (HAVE_UNION_WAIT)
 #include <sys/wait.h>
 #endif
 
@@ -596,8 +596,8 @@ start_job_command (child)
      can log the working directory before the command's own error messages
      appear.  */
 
-  message ((just_print_flag || (!(flags & COMMANDS_SILENT) && !silent_flag))
-	   ? (char *) 0 : "%s", p);
+  message (0, (just_print_flag || (!(flags & COMMANDS_SILENT) && !silent_flag))
+	   ? "%s" : (char *) 0, p);
 
   /* Tell update_goal_chain that a command has been started on behalf of
      this target.  It is important that this happens here and not in
