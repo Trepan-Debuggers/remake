2297db6c52d710025c666d3c59e61a23d5662be2
roland <roland>
1996-06-22 19:30:04 +0000

    Wed May 15 17:37:26 1996  Roland McGrath  <roland@delasyd.gnu.ai.mit.edu>
    	* dir.c (print_dir_data_base): Use %ld dev and ino and cast them to
    	long.

--- dir.c
+++ dir.c
@@ -785,8 +785,9 @@ print_dir_data_base ()
 		  dir->contents->ino[0], dir->contents->ino[1],
 		  dir->contents->ino[2]);
 #else
-	  printf ("# %s (device %d, inode %d): could not be opened.\n",
-		  dir->name, dir->contents->dev, dir->contents->ino);
+	  printf ("# %s (device %ld, inode %ld): could not be opened.\n",
+		  dir->name, (long int) dir->contents->dev,
+		  (long int) dir->contents->ino);
 #endif
 #endif /* WIN32 */
 	else
