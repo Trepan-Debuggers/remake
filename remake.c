4aa53029951392bba5f86c1776ce10f283317a5f
roland <roland>
1996-07-14 19:59:04 +0000

    Sun Jul 14 12:59:27 1996  Roland McGrath  <roland@delasyd.gnu.ai.mit.edu>
    	* remake.c (f_mtime): Don't compare MTIME to NOW if MTIME == -1.

--- remake.c
+++ remake.c
@@ -1030,27 +1030,18 @@ f_mtime (file, search)
        unrecoverably absent.  */
 
     static time_t now;
-#ifdef VMS
-    /* Handle vms 64bit to 32bit time hack introduced in vms_stat() ... */
-    static unsigned long vms_now[2]; /* assumes 32 bit long ! */
-    static int vms_now_set = 0;
-
-    if (!vms_now_set)
-      {
-	sys$gettim(vms_now);
-	now = ((vms_now[0]>>24) & 0xff) + ((vms_now[1]<<8) & 0xffffff00);
-	vms_now_set = 1;
-      }
-#endif
-    if (mtime > now && ! file->updated)
+    if (mtime != -1 && mtime > now && ! file->updated)
       {
 	/* This file's time appears to be in the future.
 	   Update our concept of the present, and compare again.  */
-#ifndef VMS
+#ifdef VMS
+	/* Handle vms 64bit to 32bit time hack introduced in vms_stat ... */
+	static unsigned long vms_now[2]; /* assumes 32 bit long ! */
+	sys$gettim (vms_now);
+	now = ((vms_now[0]>>24) & 0xff) + ((vms_now[1]<<8) & 0xffffff00);
+#else
 	extern time_t time ();
 	if (mtime > time (&now))
-#else
-	if ((mtime != -1) && (mtime > now))
 #endif
 	  {
 	    error ("*** File `%s' has modification time in the future",
