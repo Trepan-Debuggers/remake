019f33835dbf19ab908ff0e955a007c90a54a0db
roland <roland>
1996-08-27 11:29:03 +0000

    Tue Aug 27 01:06:34 1996  Roland McGrath  <roland@baalperazim.frob.com>
    	* main.c (print_version): Print out bug-reporting address.
    Mon Aug 26 19:55:47 1996  Roland McGrath  <roland@baalperazim.frob.com>
    
    	* main.c (print_data_base): Don't declare ctime; headers do it for us
     	already.

--- main.c
+++ main.c
@@ -2141,7 +2141,9 @@ print_version ()
 %s\tFree Software Foundation, Inc.\n\
 %sThis is free software; see the source for copying conditions.\n\
 %sThere is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A\n\
-%sPARTICULAR PURPOSE.\n\n", precede, precede, precede, precede, precede);
+%sPARTICULAR PURPOSE.\n\n\
+%sReport bugs to <bug-gnu-utils@prep.ai.mit.edu>.\n\n",
+	  precede, precede, precede, precede, precede, precede);
 
   printed_version = 1;
 
@@ -2155,7 +2157,6 @@ print_version ()
 static void
 print_data_base ()
 {
-  extern char *ctime ();
   time_t when;
 
   when = time ((time_t *) 0);
