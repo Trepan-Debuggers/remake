a37e344389fd48d08a78817427b45aabbfb3efda
psmith <psmith>
1999-04-13 02:28:27 +0000

    Clean up some logic.

--- main.c
+++ main.c
@@ -1564,41 +1564,39 @@ int main (int argc, char ** argv)
     else
       lastgoal->next = 0;
 
-    if (goals != 0)
+    if (!goals)
       {
-	/* Update the goals.  */
+        if (read_makefiles == 0)
+          fatal (NILF, "No targets specified and no makefile found");
 
-	if (debug_flag)
-	  puts ("Updating goal targets....");
+        fatal (NILF, "No targets");
+      }
 
-	switch (update_goal_chain (goals, 0))
-	  {
-	  case -1:
-	    /* Nothing happened.  */
-	  case 0:
-	    /* Updated successfully.  */
-	    status = EXIT_SUCCESS;
-	    break;
-	  case 2:
-	    /* Updating failed.  POSIX.2 specifies exit status >1 for this;
-	       but in VMS, there is only success and failure.  */
-	    status = EXIT_FAILURE ? 2 : EXIT_FAILURE;
-	    break;
-	  case 1:
-	    /* We are under -q and would run some commands.  */
-	    status = EXIT_FAILURE;
-	    break;
-	  default:
-	    abort ();
-	  }
-      }
-    else
-      {
-	if (read_makefiles == 0)
-	  fatal (NILF, "No targets specified and no makefile found");
-	else
-	  fatal (NILF, "No targets");
-      }
+    /* Update the goals.  */
+
+    if (debug_flag)
+      puts ("Updating goal targets....");
+
+    switch (update_goal_chain (goals, 0))
+    {
+      case -1:
+        /* Nothing happened.  */
+      case 0:
+        /* Updated successfully.  */
+        status = EXIT_SUCCESS;
+        break;
+      case 2:
+        /* Updating failed.  POSIX.2 specifies exit status >1 for this;
+           but in VMS, there is only success and failure.  */
+        status = EXIT_FAILURE ? 2 : EXIT_FAILURE;
+        break;
+      case 1:
+        /* We are under -q and would run some commands.  */
+        status = EXIT_FAILURE;
+        break;
+      default:
+        abort ();
+    }
 
     /* If we detected some clock skew, generate one last warning */
     if (clock_skew_detected)
