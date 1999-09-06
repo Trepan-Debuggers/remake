ff99ca8b77a93f5e256dec327c00bc1f753ddcda
psmith <psmith>
1999-09-06 05:21:52 +0000

    * Some doc and help changes.

--- main.c
+++ main.c
@@ -1300,8 +1300,9 @@ int main (int argc, char ** argv)
 #ifdef __MSDOS__
   if (job_slots != 1)
     {
-      error (NILF, _("Parallel jobs (-j) are not supported on MS-DOS."));
-      error (NILF, _("Resetting to single job mode."));
+      error (NILF,
+             _("Parallel jobs (-j) are not supported on this platform."));
+      error (NILF, _("Resetting to single job (-j1) mode."));
       job_slots = 1;
     }
 #endif
