19bfec27ab6797647cb1bb0fee9a2c048cbca9e5
psmith <psmith>
1999-09-22 05:57:52 +0000

    * Oops.  Fix a problem running submakes like $(MAKE) $(MFLAGS).

--- main.c
+++ main.c
@@ -1321,8 +1321,9 @@ int main (int argc, char ** argv)
   {
     char *cp;
 
-    if (jobserver_fds->idx > 1)
-      fatal (NILF, _("internal error: multiple --jobserver-fds options"));
+    for (i=1; i < jobserver_fds->idx; ++i)
+      if (!streq (jobserver_fds->list[0], jobserver_fds->list[i]))
+        fatal (NILF, _("internal error: multiple --jobserver-fds options"));
 
     /* Now parse the fds string and make sure it has the proper format.  */
 
