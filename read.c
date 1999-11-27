3b2305b61103d8c0e4b357a6b0bd11c94111de54
psmith <psmith>
1999-11-27 08:09:42 +0000

    * Update debugging to use string flags instead of integers.

--- read.c
+++ read.c
@@ -329,7 +329,7 @@ read_makefile (filename, flags)
   pattern_percent = 0;
   cmds_started = fileinfo.lineno;
 
-  if (ISDB (DB_EXTRA))
+  if (ISDB (DB_VERBOSE))
     {
       printf (_("Reading makefile `%s'"), fileinfo.filenm);
       if (flags & RM_NO_DEFAULT_GOAL)
