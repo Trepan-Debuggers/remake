75e679dff7c496103098c153e4dfaf1a8469fb5f
psmith <psmith>
2002-07-10 12:59:07 +0000

    Implement SysV-style $$@ support.  I looked at E.Parmelan's patch but decided to implement this a different way, and didn't use it.

--- main.c
+++ main.c
@@ -2169,9 +2169,9 @@ print_usage (bad)
     }
 
   if (!remote_description || *remote_description == '\0')
-    fprintf (usageto, _("\nBuilt for %s"), make_host);
+    fprintf (usageto, _("\nThis program built for %s"), make_host);
   else
-    fprintf (usageto, "\nBuilt for %s (%s)", make_host, remote_description);
+    fprintf (usageto, "\nThis program built for %s (%s)", make_host, remote_description);
 
   fprintf (usageto, _("\nReport bugs to <bug-make@gnu.org>\n"));
 }
