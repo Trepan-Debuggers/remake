22f9c4511c28f1fa7e90fcb88ff6f4080d0fa018
psmith <psmith>
2004-02-25 19:30:27 +0000

    Fix the origin regression test. Remove sample code from make.h I accidentally left behind.

--- make.h
+++ make.h
@@ -387,21 +387,6 @@ struct floc
 #define STRING_SIZE_TUPLE(_s) (_s), (sizeof (_s)-1)
 
 
-/* Fancy processing for variadic functions in both ANSI and pre-ANSI
-   compilers.  */
-#if HAVE_STDARG_H
-#  include <stdarg.h>
-#  define VA_START(a, f)        va_start(a, f)
-#else
-#  if HAVE_VARARGS_H
-#    include <varargs.h>
-#    define VA_START(a, f)      va_start(a)
-#  endif
-#endif
-#ifndef VA_START
-  error no variadic api
-#endif
-
 /* We have to have both stdarg.h or varargs.h AND v*printf or doprnt to use
    variadic versions of these functions.  */
 
