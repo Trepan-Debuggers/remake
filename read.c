5617e4947ea51bd984cb5c4d3f6c1989fa09276f
bosk <bosk>
2005-03-03 17:39:48 +0000

    Fixed stem termination and stem triple-expansion bugs.

--- read.c
+++ read.c
@@ -1911,32 +1911,17 @@ record_files (struct nameseq *filenames, char *pattern, char *pattern_percent,
 	    }
 	  else
 	    {
-	      /* We use patsubst_expand to do the work of translating
-		 the target pattern, the target's name and the dependencies'
-		 patterns into plain dependency names.  */
+	      /* We use subst_expand to do the work of translating
+		 % to $* in the dependency line.  */
 
               if (this != 0 && find_percent (this->name) != 0)
                 {
-                  PATH_VAR (stem);
                   char *o;
                   char *buffer = variable_expand ("");
 
-                  o = patsubst_expand (buffer, name, pattern, "%",
-                                       pattern_percent + 1, 0);
+                  o = subst_expand (buffer, this->name, "%", "$*",
+                                    1, 2, 0);
 
-
-                  strncpy (stem, buffer, o - buffer);
-                  stem[o - buffer] = '\0';
-
-                  o = subst_expand (buffer, this->name, "%", stem,
-                                    1, strlen (stem), 0);
-
-                  /* If the name expanded to the empty string, that's
-                     illegal.  */
-                  if (o == buffer)
-                    fatal (flocp,
-                           _("target `%s' leaves prerequisite pattern empty"),
-                           name);
 		  free (this->name);
 		  this->name = savestring (buffer, o - buffer);
 		}
