620f049e44a0081177d5304bb35dee719ab56029
R. Bernstein <rocky@gnu.org>
2005-11-27 12:32:17 +0000

    Add configure (with --enable-maintainer-mode) to initial setup process.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#  $Id: autogen.sh,v 1.1 2004/06/11 00:03:31 rockyb Exp $
+#  $Id: autogen.sh,v 1.2 2005/11/27 12:32:17 rockyb Exp $
 #  
 
 # This needs to be run the first time you check this project out, 
@@ -10,4 +10,18 @@
 echo You may get warnings here about missing files like README, etc.
 echo Ignore them, they are harmless.
 
+srcdir=`dirname $0`
+
 autoreconf --install --symlink --verbose
+
+conf_flags="--enable-maintainer-mode" # --enable-compile-warnings #--enable-iso-c
+
+if test x$NOCONFIGURE = x; then
+  echo Running $srcdir/configure $conf_flags "$@" ...
+  $srcdir/configure $conf_flags "$@" \
+  && echo Now type \`make\' to compile $PKG_NAME
+else
+  echo Skipping configure process.
+fi
+
+# FIXME: create po/*.po so we can remove them from CVS.
