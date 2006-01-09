9a7361df38aea1450e823b711be5cf0019f89a9a
R. Bernstein <rocky@gnu.org>
2006-01-09 06:34:59 +0000

    automess adjustment.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-#  $Id: autogen.sh,v 1.3 2006/01/09 05:35:08 rockyb Exp $
-echo "Invoking automake explicitly because autoreconf doesn't find config/elisp-comp"
+#  $Id: autogen.sh,v 1.4 2006/01/09 06:34:59 rockyb Exp $
+# Tried autoreconf. It didn't work. 
+#   --install didn't add --add-missing to automake
+#   and didn't invocate autopoint and autoheader 
+# I'm sure it must somehow my fault and not autoreconf's.
+# So for now don't use autoreconf.
 set -x
+autopoint
+aclocal -I . -I config
+autoheader
 automake --add-missing
-autoreconf --install $*
+autoconf
