fe19d97b1d3c54aaa9bf6713dd0b08a3819a4c03
eliz <eliz>
2007-12-22 11:27:02 +0000

    configh.dos.template [__DJGPP__]: Replace HAVE_SYS_SIGLIST with HAVE_DECL_SYS_SIGLIST.
    job.c (child_execute_job): Remove __MSDOS__ because MSDOS/DJGPP build does
    not use child_execute_job.
    
    variable.c (define_automatic_variables) [__MSDOS__]: Always export the SHELL
    environment variable to the child.

--- job.c
+++ job.c
@@ -1901,7 +1901,7 @@ start_waiting_jobs (void)
 #ifndef WINDOWS32
 
 /* EMX: Start a child process. This function returns the new pid.  */
-# if defined __MSDOS__ || defined __EMX__
+# if defined __EMX__
 int
 child_execute_job (int stdin_fd, int stdout_fd, char **argv, char **envp)
 {
