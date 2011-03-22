b6d1a8bf3de16d4a22fdd45dbe072d89cea1678d
rocky <rockyb@gnu.org>
2011-03-22 18:28:41 -0400

    Start to add breakpoints types: all, run, prerequisite, end

--- tests/spec/test-break.rb
+++ tests/spec/test-break.rb
@@ -3,16 +3,18 @@ require_relative 'helper'
 
 describe "breakpoints" do
   include RemakeTestHelper
-  it 'should be able to do set a breakpoint on a target (no options)' do
-    opts = {
-      :filter => Filter_filename,
-      :flags  =>'-X -f',
-      :input  => "echo 'break third
+  it 'should be able to do set a breakpoint on a target' do
+    ['end', 'prereq', 'run'].each do |break_opt|
+      opts = {
+        :filter => Filter_filename,
+        :flags  =>'-X -f',
+        :input  => "echo 'break third #{break_opt}
 continue
 quit
 '"
-    }
-    test_name = File.basename(__FILE__, '.rb')[5..-1]
-    run_remake(test_name, opts, 'triple')
+      }
+      test_name = File.basename(__FILE__, '.rb')[5..-1]
+      run_remake("#{test_name}-#{break_opt}", opts, 'triple')
+    end
   end
 end
