a46f7c6d1099101eb1462a71d36bba076b99630b
rocky <rocky@gnu.org>
2020-03-04 19:27:33 -0500

    Small help doc changes.

--- src/debugger/command/help/skip.h
+++ src/debugger/command/help/skip.h
@@ -1,10 +1,5 @@
 #define skip_HELP_TEXT							\
   "Skip execution of next command or action.\n"				\
-  "variables expanded. If no target given, the basename of the current\n" \
-  "is used. If a filename is supplied it is used. If it is the string\n" \
-  "\"here\", we write the output to stdout. If no filename is\n"	\
-  "given then create the filename by prepending a directory name to\n"	\
-  "the target name and then append \".sh\".\n"				\
   "\n"									\
   "See also:\n"								\
   "---------\n"								\
--- src/debugger/command/help/write.h
+++ src/debugger/command/help/write.h
@@ -1,7 +1,9 @@
 #define write_HELP_TEXT							\
-  "writes the commands associated of a target to a file with MAKE\n"	\
-  "variables expanded. If no target given, the basename of the current\n" \
-  "is used. If a filename is supplied it is used. If it is the string\n" \
+  "Writes the commands associated of a target to a file with MAKE\n"	\
+  "variables expanded.\n"						\
+  "\n"									\
+  "If no target given, the basename of the current target is used.\n"	\
+  "If a filename is supplied it is used. If it is the string\n"		\
   "\"here\", we write the output to stdout. If no filename is\n"	\
   "given then create the filename by prepending a directory name to\n"	\
   "the target name and then append \".sh\"."
