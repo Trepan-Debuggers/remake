49ab16529d7610a68e46c7d4a74960217633b678
Victor Sergienko <singalen@gmail.com>
2020-05-10 21:23:20 -0700

    #107: Debugger "watchpoints": breakpoints that trigger when a command executed matches a particular regex.

--- libdebugger/commands.h
+++ libdebugger/commands.h
@@ -53,5 +53,6 @@ extern debug_return_t dbg_cmd_source(char *psz_args);
 extern debug_return_t dbg_cmd_step(char *psz_args);
 extern debug_return_t dbg_cmd_target(char *psz_args);
 extern debug_return_t dbg_cmd_up(char *psz_args);
+extern debug_return_t dbg_cmd_watch(char *psz_args);
 extern debug_return_t dbg_cmd_where(char *psz_args);
 extern debug_return_t dbg_cmd_write(char *psz_args);
