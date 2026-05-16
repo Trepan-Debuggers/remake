cd67eb1ea1936e98e8319c73f73109a3d42e2168
rocky <rocky@gnu.org>
2026-05-15 20:48:47 -0400

    Add config variables for Windows

---
+++ src/mkconfig.h
@@ -0,0 +1,37 @@
+/* src/mkconfig.h.  Generated from mkconfig.h.in by configure.  */
+/* Autoconf values for use on non-POSIX systems.
+Copyright (C) 2022 Free Software Foundation, Inc.
+This file is part of GNU Make.
+
+GNU Make is free software; you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later
+version.
+
+GNU Make is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* Name of package */
+#define PACKAGE "remake"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "https://github.com/rocky/remake/issues"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "GNU Remake"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "GNU Remake 4.4+dbg-1.6"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "remake"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "https://github.com/Trepan-Debuggers/remake/"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "4.4+dbg-1.6"
