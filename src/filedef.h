c9932b5c674aea284a5bea8324702e93781331ce
rocky <rocky@gnu.org>
2026-08-16 18:34:43 +0000

    Assume tv_nsec for POSIX nanoseconds field.

--- src/filedef.h
+++ src/filedef.h
@@ -156,7 +156,7 @@ int stemlen_compare (const void *v1, const void *v2);
 
 #if FILE_TIMESTAMP_HI_RES
 # define FILE_TIMESTAMP_STAT_MODTIME(fname, st) \
-    file_timestamp_cons (fname, (st).st_mtime, (st).ST_MTIM_NSEC)
+    file_timestamp_cons (fname, (st).st_mtime, (st).tv_nsec)
 #else
 # define FILE_TIMESTAMP_STAT_MODTIME(fname, st) \
     file_timestamp_cons (fname, (st).st_mtime, 0)
